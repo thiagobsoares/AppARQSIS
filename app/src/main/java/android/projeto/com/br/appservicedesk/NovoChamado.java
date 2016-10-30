322b2ec111f824359ffb4253fea8c89638ebe3cc
thiagobsoares <thiagobsoares@globomail.com>
2016-10-29 23:53:24 -0200

    entrega um

---
+++ app/src/main/java/android/projeto/com/br/appservicedesk/NovoChamado.java
@@ -0,0 +1,13 @@
+package android.projeto.com.br.appservicedesk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NovoChamado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_chamado);
+    }
+}
