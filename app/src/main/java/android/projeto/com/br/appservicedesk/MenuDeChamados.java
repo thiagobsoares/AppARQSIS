1950bc94b4d43026e79bdc88074321613af3d50c
thiagobsoares <thiagobsoares@globomail.com>
2016-10-29 23:55:41 -0200

    entrega dois

--- app/src/main/java/android/projeto/com/br/appservicedesk/MenuDeChamados.java
+++ app/src/main/java/android/projeto/com/br/appservicedesk/MenuDeChamados.java
@@ -1,7 +1,9 @@
 package android.projeto.com.br.appservicedesk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuDeChamados extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class MenuDeChamados extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_de_chamados);
     }
+
+    public void novoChamado(View view){
+        Intent intent = new Intent(this,NovoChamado.class);
+        startActivity(intent);
+    }
+
+    public void consultarChamados(View view){
+        Intent intent = new Intent(this,ListarChamados.class);
+        startActivity(intent);
+    }
+
+    public void fecharChamados(View view){
+        Intent intent = new Intent(this,FecharChamados.class);
+        startActivity(intent);
+    }
+
 }
